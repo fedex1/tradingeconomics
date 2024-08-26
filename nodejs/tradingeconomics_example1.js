21f98fd6bea08ae0d86ef7a030d1d49fb672d01f
ralph yozzo <ralph@brooklynmarathon.com>
2024-08-26 18:00:47 -0400

    ralph-desktop

--- nodejs/tradingeconomics_example1.js
+++ nodejs/tradingeconomics_example1.js
@@ -4,5 +4,6 @@ const te = require('tradingeconomics');
     te.login(process.env.TRADING_ECONOMICS_APIKEY)
     // const data = await te.getHistoricalData(country = 'United States', indicator = 'Money Supply M2', start_date = '2015-01-01') 
     const data = await te.getHistoricalData(country = 'Sweden', indicator = 'Money Supply M2', start_date = '2015-01-01')
-    console.log(data)
+    // console.log(data)
+    console.log(JSON.stringify(data,"",3));
 })();
